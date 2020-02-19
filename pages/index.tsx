ab6e8ddb89f9576ad315abaf0aeff574bb64cf46
Benjamin Tang <benjamin.tang.hz@gmail.com>
2020-02-19 23:36:26 +0800

    added subscription form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
+import React, { useState } from "react";
 import Head from "next/head";
 import Div100vh from "react-div-100vh";
 import { FiMail } from "react-icons/fi";
+import MailchimpSubscribe from "react-mailchimp-subscribe";
+
+const url =
+  "https://fitness.us4.list-manage.com/subscribe/post?u=50e805064958502d88ae939bc&id=36c73f6366";
 
 const Home = () => {
   return (
@@ -8,13 +13,6 @@ const Home = () => {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
-        <script
-          id="mcjs"
-          dangerouslySetInnerHTML={{
-            __html: `
-            !function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/50e805064958502d88ae939bc/5c9f3c37fc62ab9ca0b9af3a7.js");`
-          }}
-        />
       </Head>
       <Div100vh>
         <div className="flex flex-col h-full">
@@ -37,34 +35,79 @@ const Home = () => {
                 </div>
                 <p id="subtitle" className="text-lg mt-4 md:mt-8 md:text-2xl">
                   Workouts can be a great way to meet new people with common
-                  interests.
+                  interests.&nbsp;
                   <span className="hidden md:inline-flex">
-                    &nbsp;Find your squad while doing the activities you love.
+                    Find your squad while doing the activities you love.
                   </span>
                 </p>
               </div>
             </div>
-            <div
-              id="signup"
-              className="mb-8 border-b-2 border-gray-500 flex items-center py-2 px-1"
-            >
-              <FiMail size={40} className="text-gray-700 -mb-1" />
-              <input
-                type="email"
-                className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none -mb-1"
-                placeholder="Join our pre-launch tests"
-              />
-              <button
-                className="flex-shrink-0 bg-blue-500 hover:bg-blue-700  text-white py-1 px-3 rounded-lg"
-                type="button"
-              >
-                Submit
-              </button>
-            </div>
+            <MailchimpSubscribe
+              url={url}
+              render={({ subscribe, status, message }) => (
+                <CustomForm
+                  status={status}
+                  message={message}
+                  onValidated={formData => subscribe(formData)}
+                />
+              )}
+            />
           </div>
         </div>
       </Div100vh>
     </>
+  );
+};
+
+const CustomForm = ({ status, message, onValidated }) => {
+  let email;
+  const submit = () =>
+    email &&
+    email.value.indexOf("@") > -1 &&
+    onValidated({
+      EMAIL: email.value
+    });
+
+  return (
+    <div
+      id="signup"
+      className="mb-8 border-b-2 border-gray-500 flex items-center py-2 px-1"
+    >
+      {status === "sending" && (
+        <div className="text-blue-700 text-center w-full">Submitting...</div>
+      )}
+      {status === "error" && (
+        <div className="text-red-700 text-center w-full">
+          You are already subscribed!
+        </div>
+      )}
+      {status === "success" && (
+        <div
+          className="text-green-700 text-center w-full"
+          dangerouslySetInnerHTML={{ __html: message }}
+        >
+          Success! Welcome to the Squad!
+        </div>
+      )}
+      {!status && (
+        <>
+          <FiMail size={40} className="text-gray-700 -mb-1" />
+          <input
+            ref={node => (email = node)}
+            type="email"
+            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none -mb-1"
+            placeholder="Join our pre-launch tests"
+          />
+          <button
+            className="flex-shrink-0 bg-blue-500 hover:bg-blue-700  text-white py-1 px-3 rounded-lg"
+            type="button"
+            onClick={submit}
+          >
+            Submit
+          </button>
+        </>
+      )}
+    </div>
   );
 };
 
