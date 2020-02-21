1326fe7e65962a9d6e4bc74de7bdc72bdb6f34ce
Benjamin Tang <benjamin.tang.hz@gmail.com>
2020-02-21 11:11:48 +0800

    added segment script

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,6 +36,14 @@ const Home = () => {
             __html: `(function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm_share", b="https://embed.typeform.com/"; if(!gi.call(d,id)){ js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })()`
           }}
         ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
+            analytics.load("rc4dK0bDR9lfm7R0gUU4m0igIrJYsOpb");
+            analytics.page();
+            }}();`
+          }}
+        ></script>
       </Head>
       <Div100vh>
         <div className="flex flex-col h-full">
